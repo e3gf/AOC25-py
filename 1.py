37669a5a22d2c0a02b01dbd12d154427cee2326c
e3gf <ywsttigtbtgrihh@proton.me>
2025-12-01 09:11:35 -0800

    Refactored the code and split part 1 and 2 properly

--- 1.py
+++ 1.py
@@ -2,26 +2,26 @@ lines = ""
 with open("1.txt", "r") as f:
     lines = f.read().split('\n')
 
-current, count1, count2 = 50, 0, 0
+current1, current2, count1, count2 = 50, 50, 0, 0
 for line in lines:
     if line == "":
         continue
     dir = line[0]
     num = int(line[1:])
-    
-    # --- Part 2 --- (handles the overflow and underflow for part 1 too)
-    if dir == "R":
-        current += num
-        count2 += current // 100
-        current %= 100
-    elif dir == "L":
-        current -= num
-        count2 += abs(((current + 99) if current + num == 0 else (current - 1)) // 100)
-        current %= 100
 
     # --- Part 1 ---
-    if current == 0:
+    current1 = (current1 + num) % 100 if dir == "R" else (current1 - num) % 100
+    if current1 == 0:
         count1 += 1
+
+    # --- Part 2 ---
+    if dir == "R":
+        current2 += num
+        count2 += current2 // 100
+    else:
+        current2 -= num
+        count2 += abs((current2 + 99) // 100 if current2 + num == 0 else (current2 - 1) // 100)
+    current2 %= 100
 
 print("Part 1:", count1)
 print("Part 2:", count2)
